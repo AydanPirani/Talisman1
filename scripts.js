47445f510efa691c08fe4baaf05f8a4db820ca98
AydanPirani <aydanpirani@gmail.com>
2020-10-27 00:21:33 -0700

    Finalized maze-generation algo.

--- scripts.js
+++ scripts.js
@@ -4,7 +4,7 @@ randomize();
 
 x = 1;
 y = Math.round(y_cells / 2);
-move(x,y)
+move(x, y)
 
 document.onkeydown = check_key;
 
@@ -28,21 +28,23 @@ function check_key(event) {
   if (document.getElementById(n_x + "," + n_y).innerHTML == '<div class="wall"></div>') {
     //hit wall
     t.className = "shake-hard shake-constant";
-    setTimeout(function(){ t.className = "none"; }, 500);
+    setTimeout(function() {
+      t.className = "none";
+    }, 500);
   } else {
-    change_to_empty(x,y)
-    move(n_x,n_y)
+    change_to_empty(x, y)
+    move(n_x, n_y)
     x = n_x;
     y = n_y;
   }
 
 }
 
-function randomize(){
+function randomize() {
   x = 1;
   y = Math.round(y_cells / 2);
   generate_table(x_cells, y_cells)
-  generate_walls([0,y_cells],[x_cells,0])
+  generate_walls([1, y_cells], [x_cells, 1])
 }
 
 function generate_table(x, y) {
@@ -51,7 +53,7 @@ function generate_table(x, y) {
   for (var i = 1; i < y + 1; i++) {
     temp += "<tr>"
     for (var j = 1; j < x + 1; j++) {
-      if (j == 1 || j == x || i == 1 || i == y){
+      if (j == 1 || j == x || i == 1 || i == y) {
         c = "wall"
       } else {
         c = "empty"
@@ -64,35 +66,53 @@ function generate_table(x, y) {
 }
 
 function generate_walls(LL, UR) {
-  x_l = Math.abs(LL[0]-UR[0])
-  y_l = Math.abs(LL[1]-UR[1])
+  // console.log(LL + "\n" + UR)
+  x_l = Math.abs(LL[0] - UR[0])
+  y_l = Math.abs(LL[1] - UR[1])
 
-  x_coord = Math.round(x_l * Math.random())
-  while (x_coord<3||x_coord>x_cells-3||is_wall(x_coord-2,2)&&is_wall(x_coord-2,3)||is_wall(x_coord-1,2)&&is_wall(x_coord-1,3)||is_wall(x_coord+0,2)&&is_wall(x_coord+0,3)||is_wall(x_coord+1,2)&&is_wall(x_coord+1,3)||is_wall(x_coord+2,2)&&is_wall(x_coord+2,3)) {
-    x_coord = Math.round(x_l * Math.random())
-  }
-  x_coord += LL[0]
+  x_coord = Math.round(x_l * Math.random()) + LL[0]
+  y_coord = Math.round(y_l * Math.random()) + UR[1]
 
-  y_coord = Math.round(y_l * Math.random())
-  while (y_coord<3||y_coord>y_cells-3||is_wall(y_coord-2,2)&&is_wall(y_coord-2,3)||is_wall(y_coord-1,2)&&is_wall(y_coord-1,3)||is_wall(y_coord+0,2)&&is_wall(y_coord+0,3)||is_wall(y_coord+1,2)&&is_wall(y_coord+1,3)||is_wall(y_coord+2,2)&&is_wall(y_coord+2,3)) {
-    y_coord = Math.round(y_l * Math.random())
-  }
-  y_coord += LL[0]
-
-
-  //Changes a whole column to a wall
-  for (var i = 1; i < y_cells; i++) {
-    change_to_wall(x_coord,i)
-  }
+  x_c = x_coord>2&&Math.abs(x_coord - LL[0]) > 2 && Math.abs(x_coord - UR[0]) > 2
+  y_c = y_coord!=13&&Math.abs(y_coord - LL[1]) > 2 && Math.abs(y_coord - UR[1]) > 2
 
   //Changes a whole row to a wall
-  for (var i = 1; i < x_cells; i++) {
-    change_to_wall(i,y_coord)
+  if (x_c) {
+    for (var i = UR[1]; i < LL[1]; i++) {
+      change_to_wall(x_coord, i)
+    }
   }
 
+  //Changes a whole column to a wall
+  if (y_c) {
+    for (var i = LL[0]; i < UR[0]; i++) {
+      change_to_wall(i, y_coord)
+    }
+  }
+
+  if (x_c && y_c) {
+    console.log("A")
+    generate_walls([LL[0], y_coord],[x_coord,UR[1]])
+    generate_walls(LL,[x_coord, y_coord])
+    generate_walls([x_coord, y_coord],UR)
+    generate_walls([x_coord, LL[1]],[UR[0], y_coord])
+  } else if (x_c) {
+    console.log("B")
+    generate_walls(LL,[x_coord, UR[1]])
+    generate_walls([x_coord, LL[1]],UR)
+  } else if (y_c) {
+    console.log("C")
+    generate_walls([LL[0],y_coord],UR)
+    generate_walls(LL,[UR[0],y_coord])
+  }
+
+
+  // console.log(x_coord + "," + y_coord)
 }
 
+
 function is_wall(x, y) {
+  console.log(x + "," + y)
   return document.getElementById(x + "," + y).innerHTML == '<div class="wall"></div>';
 }
 
@@ -105,6 +125,6 @@ function change_to_empty(x, y) {
   document.getElementById(x + "," + y).innerHTML = '<div class="empty"></div>';
 }
 
-function move(x,y) {
+function move(x, y) {
   document.getElementById(x + "," + y).innerHTML = '<div class="user"></div>';
 }
