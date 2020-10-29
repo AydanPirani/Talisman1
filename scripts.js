9a273037e022c4b97efeb6fe688c01f28c559885
AydanPirani <aydanpirani@gmail.com>
2020-10-29 00:47:36 -0700

    Added avatar selection screen.

--- scripts.js
+++ scripts.js
@@ -1,12 +1,23 @@
 x_cells = Math.round(screen.width / 30)
 y_cells = Math.round(screen.height / 30)
+document.getElementById("choose").style.display = "block";
 randomize();
-
 x = 1;
 y = Math.round(y_cells / 2);
+color = ""
+while(color != ""){
+  setTimeout(function() {console.log("waiting") }, 500);
+}
 move(x, y)
 
 document.onkeydown = check_key;
+
+function chose() {
+  color = document.getElementById("color").value
+  console.log(color)
+  document.getElementById("choose").style.display = "none"
+  document.querySelector(".user").style.backgroundColor = color
+}
 
 //Event Listeners to check which keys were pressed
 function check_key(event) {
@@ -73,8 +84,8 @@ function generate_walls(LL, UR) {
   x_coord = Math.round(x_l * Math.random()) + LL[0]
   y_coord = Math.round(y_l * Math.random()) + UR[1]
 
-  x_c = x_coord>2&&Math.abs(x_coord - LL[0]) > 2 && Math.abs(x_coord - UR[0]) > 2
-  y_c = y_coord!=13&&Math.abs(y_coord - LL[1]) > 2 && Math.abs(y_coord - UR[1]) > 2
+  x_c=(x_coord>2&&Math.abs(x_coord-LL[0])>2&&Math.abs(x_coord-UR[0])>2)
+  y_c=(y_coord!=13&&Math.abs(y_coord-LL[1])>2&&Math.abs(y_coord-UR [1])>2)
 
   //Changes a whole row to a wall
   if (x_c) {
@@ -95,17 +106,14 @@ function generate_walls(LL, UR) {
   }
 
   if (x_c && y_c) {
-    console.log("A")
     generate_walls([LL[0], y_coord],[x_coord,UR[1]])
     generate_walls(LL,[x_coord, y_coord])
     generate_walls([x_coord, y_coord],UR)
     generate_walls([x_coord, LL[1]],[UR[0], y_coord])
   } else if (x_c) {
-    console.log("B")
     generate_walls(LL,[x_coord, UR[1]])
     generate_walls([x_coord, LL[1]],UR)
   } else if (y_c) {
-    console.log("C")
     generate_walls([LL[0],y_coord],UR)
     generate_walls(LL,[UR[0],y_coord])
   }
@@ -129,9 +137,10 @@ function change_to_empty(x, y) {
   document.getElementById(x + "," + y).innerHTML = '<div class="empty"></div>';
 }
 
+// TODO: COLOR CHOICE
 function move(x, y) {
   generate_table(x_cells,y_cells)
   generate_walls([1, y_cells], [x_cells, 1])
   document.getElementById(x + "," + y).innerHTML = '<div class="user"></div>';
-
+  document.querySelector(".user").style.backgroundColor = color
 }
